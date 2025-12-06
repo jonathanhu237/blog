eff336ff1b21cbf6afcd56ea334ec4e3a5c6bf70
jonathanhu237 <jonathanhu237@gmail.com>
2025-12-06 22:10:21 +0800

    chore: add deploy command
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,18 @@ import tailwindcss from '@tailwindcss/vite';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex]
-  }
+  },
+
+  adapter: cloudflare()
 });
